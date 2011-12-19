72af23dc522045bf1ce4806e904412b48c7d33c8
stefan <stefansbv@users.sourceforge.net>
2011-12-19 21:18:53 +0200

    Back to manual prereqs.

--- lib/TpdaQrt.pm
+++ lib/TpdaQrt.pm
@@ -12,11 +12,11 @@ TpdaQrt::Db - Tpda TpdaQrt database operations module
 
 =head1 VERSION
 
-Version 0.30
+Version 0.31
 
 =cut
 
-our $VERSION = '0.30';
+our $VERSION = '0.31';
 
 =head1 SYNOPSIS
 
